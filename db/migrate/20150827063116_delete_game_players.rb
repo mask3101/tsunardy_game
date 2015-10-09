432825b9733b2833d1a6b21b0e7af63de92774f8
Alejandro San Juan <mask3101@gmail.com>
2015-10-09 00:05:21 -0500

    se quitaron migraciones no necesarias

--- db/migrate/20150827063116_delete_game_players.rb
+++
@@ -1,6 +0,0 @@
-class DeleteGamePlayers < ActiveRecord::Migration
-  def change
-    remove_column :games, :p1, :boolean
-    remove_column :games, :p2, :boolean
-  end
-end
--- db/migrate/20150827063123_add_game_players.rb
+++
@@ -1,6 +0,0 @@
-class AddGamePlayers < ActiveRecord::Migration
-  def change
-    add_column :games, :p1, :integer
-    add_column :games, :p2, :integer
-  end
-end
